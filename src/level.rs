fee204e03601f71df23e8f9f5988ce950f50dd69
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2024-04-24 15:46:11 -0400

    Fully finished conversion from screen space to xyz index space, now we can get indices in O(1) time directly from mouse position!

--- src/level.rs
+++ src/level.rs
@@ -3,15 +3,18 @@ use bevy::core_pipeline::core_3d::Camera3dBundle;
 use bevy::ecs::event::EventReader;
 use bevy::ecs::system::{Commands, Query, ResMut, Resource};
 use bevy::input::mouse::MouseMotion;
-use bevy::math::{vec3, Vec3};
+use bevy::math::{vec3, Vec2, Vec3};
 use bevy::pbr::{DirectionalLight, DirectionalLightBundle};
 use bevy::prelude::default;
 use bevy::render::camera::{OrthographicProjection, Projection, ScalingMode};
 use bevy::render::mesh::Mesh;
 use bevy::scene::{Scene, SceneBundle};
 use bevy::transform::components::Transform;
+use bevy::transform::components::GlobalTransform;
 use bevy::ecs::system::Res;
 use bevy::window::{CursorMoved, Window};
+use bevy::render::camera::Camera;
+use bevy::ecs::query::With;
 
 #[derive(Copy, Clone)]
 pub struct Vertex 
@@ -23,6 +26,14 @@ pub struct Vertex
 
 const MAP_HEIGHT : i8 = 10; 
 const MAP_WIDTH : i8 = 10; 
+
+const HEX_RADIUS : f32 = 2.0f32;
+const J_OFFSET : f32 = HEX_RADIUS - 0.90f32;
+const J_SPACING : f32 = HEX_RADIUS + 0.22f32;
+const INITIAL_TRANSLATION : Vec3 = Transform::from_xyz(-4.0f32, 0.0f32, 7.0f32).translation;
+
+const X_BASIS_VECTOR : Vec2 = Vec2::new(-HEX_RADIUS * 0.3f32, -HEX_RADIUS/2.0f32);
+const Z_BASIS_VECTOR : Vec2 = Vec2::new (HEX_RADIUS * 0.3f32, HEX_RADIUS/2.0f32);
 
 #[derive(Resource)]
 pub struct Map
@@ -40,9 +51,6 @@ impl Map
     pub fn spawn(&mut self, command_queue : &mut Commands, asset_server : Res<AssetServer>)
     {
         let hexagon : bevy::prelude::Handle<Scene> = asset_server.load("Hex.glb#Scene0");
-
-        let hex_radius : f32 = 2.0f32;
-        let initial_translation : Vec3 = Transform::from_xyz(-4.0f32, 0.0f32, 7.0f32).translation;
     
         let max_x : i8 = MAP_WIDTH/2 + f32::floor((MAP_HEIGHT as f32/2f32) / 2f32) as i8 + 1;
         let max_z : i8 = max_x + 1;
@@ -59,25 +67,25 @@ impl Map
 
         for j in 0..MAP_HEIGHT/2
         {
-            let offset_z : f32 = initial_translation.z + if j % 2 == 0 {hex_radius - 0.90f32} else {0.0f32};
-            let offset_x : f32 = initial_translation.x + (hex_radius) * j as f32;
+            let offset_z : f32 = INITIAL_TRANSLATION.z + if j % 2 == 0 {J_OFFSET} else {0.0f32};
+            let offset_x : f32 = INITIAL_TRANSLATION.x + (HEX_RADIUS) * j as f32;
     
             for i in 0..MAP_WIDTH/2
             {
                 command_queue.spawn(SceneBundle{
                     scene : hexagon.clone(),
-                    transform : Transform::from_xyz(offset_x, initial_translation.y,  offset_z - i as f32 * (hex_radius + 0.22f32)),
+                    transform : Transform::from_xyz(offset_x, INITIAL_TRANSLATION.y,  offset_z - i as f32 * J_SPACING),
                     ..default()
                 });
                 
-                let center : Vec3 = vec3(offset_x, initial_translation.y,  offset_z - i as f32 * (hex_radius + 0.16f32));
+                let center : Vec3 = vec3(offset_x, INITIAL_TRANSLATION.y,  offset_z - i as f32 * J_SPACING);
     
-                let bottom : Vec3 = center + vec3(hex_radius/1.9f32, 0.0f32, 0.0f32);
-                let top : Vec3 = center - vec3(hex_radius/1.9f32, 0.0f32, 0.0f32);
-                let top_right : Vec3 = center - vec3(hex_radius * 0.3f32, 0.0f32, hex_radius/2.0f32);
-                let top_left : Vec3 = center - vec3(hex_radius * 0.3f32, 0.0f32, -hex_radius/2.0f32);
-                let bottom_right : Vec3 = center - vec3(-hex_radius * 0.3f32, 0.0f32, hex_radius/2.0f32);
-                let bottom_left : Vec3 = center + vec3(hex_radius * 0.3f32, 0.0f32, hex_radius/2.0f32);
+                let bottom : Vec3 = center + vec3(HEX_RADIUS/1.9f32, 0.0f32, 0.0f32);
+                let top : Vec3 = center - vec3(HEX_RADIUS/1.9f32, 0.0f32, 0.0f32);
+                let top_right : Vec3 = center + vec3(X_BASIS_VECTOR.x, 0.0f32, -X_BASIS_VECTOR.y);
+                let top_left : Vec3 = center + vec3(X_BASIS_VECTOR.x, 0.0f32, X_BASIS_VECTOR.y);
+                let bottom_right : Vec3 = center + vec3(Z_BASIS_VECTOR.x, 0.0f32, Z_BASIS_VECTOR.y);
+                let bottom_left : Vec3 = center + vec3(Z_BASIS_VECTOR.x, 0.0f32, -Z_BASIS_VECTOR.y);
     
                 let index_to_grid : Vec3 = index_to_grid(i, j);
                 let x : usize = index_to_grid.x as usize;
@@ -199,12 +207,58 @@ pub fn index_to_grid(i : i8, j : i8) -> Vec3
     return grid_coords;
 }
 
-pub fn mouse_moved(mut cursor_event : EventReader<CursorMoved>, mut window : Query<&mut Window>, mut map : ResMut<Map>)
+pub fn world_to_index(x : f32, z : f32) -> Option<Vec3>
+{
+    let j: f32 = (x - INITIAL_TRANSLATION.x) / HEX_RADIUS;
+    let index_j: f32 = f32::round(j);
+
+    let i : f32 = ((z - INITIAL_TRANSLATION.z) - if index_j % 2f32 == 0f32 {J_OFFSET} else {0.0f32})/-J_SPACING;
+    let index_i : f32 = f32::round(i);
+
+    let offset_from_center: Vec2 = Vec2::new(i - index_i as f32, j - index_j as f32);
+
+    let mut x_index_offset: f32 = offset_from_center.x;
+    let mut y_index_offset: f32 = offset_from_center.y;
+    let mut z_index_offset: f32 = offset_from_center.x;
+
+    if z_index_offset >= 0.3f32 {z_index_offset = -1.0f32} 
+    else {z_index_offset = 0f32};
+    if offset_from_center.y <= -0.3f32 {z_index_offset = -1f32;}
+
+    if y_index_offset >= 0.1f32 {y_index_offset = 1.0f32}
+    else {y_index_offset = 0f32;}
+
+    if x_index_offset <= -0.3f32 {x_index_offset = -1.0f32} 
+    else {x_index_offset = 0f32};
+    if offset_from_center.y <= -0.3f32 {x_index_offset = -1f32;}
+
+    if (index_i < 0.0f32 || index_i > (MAP_WIDTH/2 - 1) as f32) || 
+    (index_j < 0.0f32 || index_j > (MAP_HEIGHT/2 - 1) as f32)
+    {
+        return Option::None;
+    }
+    else 
+    {
+        return Option::Some(Vec3::new(x_index_offset, y_index_offset, z_index_offset) + index_to_grid(index_i as i8, index_j as i8));
+    }
+}
+
+pub fn mouse_moved(mut cursor_event : EventReader<CursorMoved>, mut window : Query<&mut Window>, mut map : ResMut<Map>, 
+    camera: Query<(&Camera, &GlobalTransform)>)
 {
     for event in cursor_event.read()
     {
         map.test();
-        println!("Cursor moved! x-{} y-{}", event.position.x - window.single().resolution.width()/2f32, event.position.y - window.single().resolution.height()/2f32)
+        match camera.single().0.viewport_to_world(camera.single().1, event.position) {
+            Some(value) => {
+                let index_position : Option<Vec3> = world_to_index(value.origin.x, value.origin.z);
+                match index_position {
+                    Some(position) => println!("Cursor moved! x: {} y: {} z: {}", position.x, position.y, position.z),
+                    None => ()
+                }
+            },
+            None =>()
+        }
     }
 }
 
@@ -214,10 +268,7 @@ pub fn initialize_map(mut command_queue : Commands, asset_server : Res<AssetServ
     map.print_vertices();
 }
 
-pub fn spawn_lights(mut command_queue : Commands,
-    asset_server : Res<AssetServer>,
-    mut meshes : ResMut<Assets<Mesh>>
-)
+pub fn spawn_lights(mut command_queue : Commands)
 {
     command_queue.spawn(DirectionalLightBundle{
         directional_light : DirectionalLight{
