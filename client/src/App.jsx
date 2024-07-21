6066716c6c459e96e2f02dba7ccf395931fcbcc4
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2024-07-20 20:50:15 -0400

    Drew basic hex grid, also storing the hexes in a way where I can access their q and r values via their indices and vice versa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,52 @@
 import { Stage, Sprite} from '@pixi/react';
 
+//Convert one of your q or r values to it's corresponding grid index
+const CoordinateToIndex = (coordinate, gridSize) => {
+  return coordinate + gridSize;
+}
+
+//Convert one of your grid indices into it's corresponding q or r value
+const IndexToCoordinate = (index, gridSize) => {
+  return index - coordinate;
+}
+
+const DrawMap = (gridSize, xOffset, yOffset) => {
+  var gridSpacing = 60;
+  var horizSpacing = 54;
+  let hexGrid = new Array(gridSize);
+
+  for (let q = -gridSize; q < gridSize + 1; q++){
+    let startIndex = -Math.min(q + gridSize, gridSize);
+    let endIndex = Math.min(-(q - gridSize), gridSize);
+
+    let qIndex = CoordinateToIndex(q, gridSize);
+    hexGrid[qIndex] = new Array(gridSize);
+
+    for (let r = startIndex; r < endIndex + 1; r++){
+      let rIndex = CoordinateToIndex(r, gridSize);
+      hexGrid[qIndex][rIndex] = (
+        <Sprite
+        image={"/Hex.svg"}
+        scale={{ x: 0.5, y: 0.5 }}
+        anchor={0.5}
+        x={xOffset + r * horizSpacing}
+        y={yOffset + (q + r/2) * gridSpacing}
+        />
+      );
+    }
+  }
+
+  return hexGrid;
+}
+
 const App = () => {
+  let width = 800;
+  let height = 600;
   return (
     <div className='h-screen w-screen content-center'>
       <div className='flex justify-center'>
-        <Stage width={800} height={600} options={{ background: 0x1e1e1e }}>
-          <Sprite
-            image={"/react.svg"}
-            scale={{ x: 0.5, y: 0.5 }}
-            anchor={0.5}
-            x={150}
-            y={150}
-          />
+        <Stage width={width} height={height} options={{ background: 0x1e1e1e }}>
+          {DrawMap(2, width/2, height/2)}
         </Stage>
       </div>
     </div>
