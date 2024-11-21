5a889b5a905cc31413f4055ca1e4df0f5c6281b0
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2024-11-21 15:20:02 -0500

    Updated dependencies, got started with Pixi Js and Scss

--- public/src/app.ts
+++ public/src/app.ts
@@ -1,2 +1,27 @@
-const myVar : number = 100;
-console.log("This is a test  " + myVar);
+import "../styles/style.scss";
+import "../styles/style2.scss"
+
+import { Application, Graphics } from "pixi.js";
+
+(async () => {
+    const app : Application = new Application();
+    const canvas : HTMLElement | null = document.getElementById("canvas");
+    if(!canvas){
+        console.error("Error, canvas div not found!");
+        return;
+    }
+
+    await app.init({
+        resizeTo: canvas,
+        backgroundAlpha: 0.9
+    });
+
+    const rectangle : Graphics = new Graphics()
+        .rect(200, 200, 80, 80)
+        .fill({
+            color: 0xffff
+        });
+    
+    app.stage.addChild(rectangle);
+    canvas.appendChild(app.canvas);
+})();
