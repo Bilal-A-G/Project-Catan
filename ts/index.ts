7f676308042a45adc0969df30848194c50a978b4
Bilal-A-G <bilalabdulg@gmail.com>
2025-09-26 21:48:51 -0400

    Imported Pixi.js into the project, and using Vite for HMR and serving front end files
    TODO, figure out how the backend is gonna tie into all this, and redo project structure, seems like frontend should be completely separate from the backend

--- ts/index.ts
+++ ts/index.ts
@@ -1,22 +1,37 @@
 import { Application, Assets, Container, Sprite } from "pixi.js";
+
 (async () => {
-	const app = new Application();
-	await app.init({ background: "#011f", resizeTo: window });
+	const app: Application = new Application();
+	await app.init({ background: "#0000", resizeTo: window });
 	document.body.appendChild(app.canvas);
 	const container = new Container();
+
 	app.stage.addChild(container);
+
+	// Load the bunny texture
 	const texture = await Assets.load("https://pixijs.com/assets/bunny.png");
+
+	// Create a 5x5 grid of bunnies in the container
 	for (let i = 0; i < 25; i++) {
 		const bunny = new Sprite(texture);
+
 		bunny.x = (i % 5) * 40;
 		bunny.y = Math.floor(i / 5) * 40;
 		container.addChild(bunny);
 	}
+
+	// Move the container to the center
 	container.x = app.screen.width / 2;
 	container.y = app.screen.height / 2;
+
+	// Center the bunny sprites in local container coordinates
 	container.pivot.x = container.width / 2;
 	container.pivot.y = container.height / 2;
+
+	// Listen for animate update
 	app.ticker.add((time) => {
+		// Continuously rotate the container!
+		// * use delta to create frame-independent transform *
 		container.rotation -= 0.01 * time.deltaTime;
 	});
 })();
